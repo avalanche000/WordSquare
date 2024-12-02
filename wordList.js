55863ebb6376dabadbc68f2c503a69e66689908e
avalanche000 <125629389+avalanche000@users.noreply.github.com>
2024-12-02 13:07:18 -0500

    Update wordList.js

--- wordList.js
+++ wordList.js
@@ -1,1 +1,15 @@
 const wordListUrl = "https://raw.githubusercontent.com/dwyl/english-words/refs/heads/master/words_dictionary.json";
+const wordList = await loadWordList();
+
+function loadWordList() {
+  return fetch(wordListUrl, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
+}
+
+export { wordList };
